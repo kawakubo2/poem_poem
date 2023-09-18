e4d557b8d0047a95d274975c24ea9e70e3d052f6
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2023-09-18 16:00:36 +0900

    2023年09月18日授業分

--- friend/update_process.php
+++ friend/update_process.php
@@ -7,6 +7,7 @@ authenticate();
 
 try {
     $db = getDb();
+    // 作家に対する友達申請に対し"承認"または"拒否"をstatusに設定する
     $sql = "UPDATE friends
             SET status = :status
             WHERE user_id = :user_id
@@ -17,8 +18,45 @@ try {
     $stt->bindValue(':author_id', $_POST['author_id']);
     $stt->execute();
 
-    header("Location: http://" . $_SERVER['HTTP_HOST'] . "/author/update_form.php?id={$_SESSION['user']['id']}");
-
+    $sql = "SELECT count(*) AS 拒否回数
+            FROM friends
+            WHERE user_id = :user_id
+              AND status = '拒否'";
+    $stt = $db->prepare($sql);
+    $stt->bindValue(':user_id', $_POST['user_id']);
+    $stt->execute();
 } catch(PDOException $e) {
     die("エラーメッセージ: {$e->getMessage()}");
 }
+
+$row = $stt->fetch(PDO::FETCH_ASSOC);
+
+if ($row['拒否回数'] >= 10) {
+    try {
+        $db = getDb();
+        $db->beginTransaction();
+        // favoritesテーブルから削除
+        $stt = $db->prepare("DELETE FROM favorites WHERE user_id = :user_id");
+        $stt->bindValue(':user_id', $_POST['user_id']);
+        $stt->execute();
+        // commentsテーブルから削除
+        $stt = $db->prepare("DELETE FROM comments WHERE user_id = :user_id");
+        $stt->bindValue(':user_id', $_POST['user_id']);
+        $stt->execute();
+        // friendsテーブルから削除
+        $stt = $db->prepare("DELETE FROM friends WHERE user_id = :user_id");
+        $stt->bindValue(':user_id', $_POST['user_id']);
+        $stt->execute();
+        // usersテーブルから削除
+        $stt = $db->prepare("DELETE FROM users WHERE id = :user_id");
+        $stt->bindValue(':id', $_POST['user_id']);
+        $stt->execute();
+
+        $db->commit();
+    } catch(PDOException $e) {
+        print("エラーメッセージ: {$e->getMessage()}");
+        $db->rollBack();
+    }
+}
+    
+header("Location: http://" . $_SERVER['HTTP_HOST'] . "/author/update_form.php?id={$_SESSION['user']['id']}");
